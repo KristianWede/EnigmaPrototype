ed4115ea47cd39761946aadfe4e62cf6c4ae3ec7
KristianWede <kris79s4@stud.kea.dk>
2022-02-28 18:24:48 +0100

    Completed the first Method, Letter to number.

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -2,17 +2,23 @@ package com.company;
 
 public class Main {
 
-    String[] alfabet = {" ", "A", "B", "C", "D","E","F","G","H","I","J","K","L","M","N",
-            "O","P","Q","R","S","T","U","V","W","X","Y","Z","Æ","Ø","Å"};
+    char[] alfabet = {' ', 'A', 'B', 'C', 'D','E','F','G','H','I','J','K','L','M','N',
+            'O','P','Q','R','S','T','U','V','W','X','Y','Z','Æ','Å'};
+
     final int ALFABETMÆNGDE = 28;
 
     int alfabetTal = 0;
 
-    public void bogstaverTilTal(){
-
+    public int bogstaverTilTal(char inputBogstav){
+        //Går igennem alle bogstaver indtil den finder hvilket tal passer til et char i alfabet Arrayet.
         for (int i = 0;ALFABETMÆNGDE >= i; i++ ){
-
+            //Tjekker om alfabet Array er det samme som inputtet.
+            if ( alfabet[i] == inputBogstav){
+                //Returnerer tallet.
+                return i;
+            }
         }
+        return 404;
     }
     public void taltilBogstaver(){
 
@@ -20,7 +26,7 @@ public class Main {
 
     public static void main(String[] args) {
         Main obj = new Main();
-        obj.bogstaverTilTal();
+        System.out.println(obj.bogstaverTilTal('C'));
 
 
     }
