d7f75c2ef40ce46738a5ea626fceaf90193e8ae7
KristianWede <kris79s4@stud.kea.dk>
2022-02-28 18:04:26 +0100

    Added Array and first lettersToNumbers

---
+++ src/com/company/Main.java
@@ -0,0 +1,27 @@
+package com.company;
+
+public class Main {
+
+    String[] alfabet = {" ", "A", "B", "C", "D","E","F","G","H","I","J","K","L","M","N",
+            "O","P","Q","R","S","T","U","V","W","X","Y","Z","Æ","Ø","Å"};
+    final int ALFABETMÆNGDE = 28;
+
+    int alfabetTal = 0;
+
+    public void bogstaverTilTal(){
+
+        for (int i = 0;ALFABETMÆNGDE >= i; i++ ){
+
+        }
+    }
+    public void taltilBogstaver(){
+
+    }
+
+    public static void main(String[] args) {
+        Main obj = new Main();
+        obj.bogstaverTilTal();
+
+
+    }
+}
